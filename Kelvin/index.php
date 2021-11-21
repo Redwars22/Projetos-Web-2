6157a5a6405d3888c3e84eec5223d8069ca8f5b3
André Pereira <andrewpereira.megabyte@gmail.com>
2021-11-21 14:07:38 -0300

    Add files via upload

---
+++ Kelvin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>🪐️⚙️ Project Kelvin - Gerador de Planetas</title>
+    <link rel="stylesheet" href="resx/style.css">
+    <link rel="stylesheet" href="resx/layout.css">
+</head>
+<body>
+    <header>
+        <img class="logo" src="resx/kelvin.png"/>
+        <div class="">
+            <a href="">Sobre</a>
+            <a href="">Ajuda</a>
+            <a href="">Mais Projetos</a>
+            <a href="mailto:">Reporte um bug</a>
+        </div>
+    </header>
+    
+    <main>
+        <img src="resx/planet.png" width="250px"/>
+        <h1>GERADOR DE PLANETAS</h1>
+        <em>Gere um planeta aleatório com o Kelvin</em>
+        <div class="formContainer">
+            <form action="" method="post">
+                <label>INSIRA O NOME DO PLANETA</label>
+                <br/>
+                <input type="text" name="" class="">
+                <button type="submit">GERAR 🪐️</button>
+            </form>
+        </div>
+    </main>
+
+    <footer>
+        <p>
+
+        </p>
+    </footer>
+</body>
+</html>
