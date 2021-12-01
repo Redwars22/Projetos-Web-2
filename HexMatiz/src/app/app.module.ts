4fa58d6e69c32c05d289057dacee5548f489880a
André Pereira <andrewpereira.megabyte@gmail.com>
2021-12-01 17:43:41 +0000

    Add files via upload

--- HexMatiz/src/app/app.module.ts
+++ HexMatiz/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './ui/header.component';
 import { ColorComponent } from './ui/colorPreview.component';
+import { FooterComponent } from './ui/footer.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeaderComponent, ColorComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ColorComponent,
+    FooterComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
