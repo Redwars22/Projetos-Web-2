385df2e4f7f241cbed22cc3c6e1b2c697e183f58
Redwars22 <andrewpereira.megabyte@gmail.com>
2021-12-25 16:42:34 -0300

    Criei pastas de arquivos dos próximos projetos

---
+++ Kanban/src/logic.js
@@ -0,0 +1,7 @@
+/*if(localStorage.getItem('to-do') === null){
+    window.alert("ERRO: Você ainda não salvou nenhuma tarefa no ProjectKanban!!!");
+}*/
+
+function save(){
+    todo = document.getElementById().value;
+}
