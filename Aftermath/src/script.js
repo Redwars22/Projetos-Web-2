c79db852cd536a3fe8441317b12516567209f8c9
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-01-02 20:10:43 -0300

    Versão ALPHA do Aftermath

--- Aftermath/src/script.js
+++ Aftermath/src/script.js
@@ -1,9 +1,11 @@
-function aboutPanel(){
-    window.alert("");
+function aboutPanel() {
+  window.alert(
+    "ProjectAftermath. Versão 1.0.2 UPDATE 1. Desenvolvido por André Pereira(@Redwars22) usando HTML, CSS, Fluent UI e JavaScript."
+  );
 }
 
-function helpPage(){}
+function helpPage() {}
 
-function sendFeedback(){}
+function sendFeedback() {}
 
-function searchDocumentation(){}
+function searchDocumentation() {}
