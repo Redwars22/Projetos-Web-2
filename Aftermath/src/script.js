3071c0c8d94d60e33583b80827aff1da8b52bf3e
Redwars22 <andrewpereira.megabyte@gmail.com>
2022-01-03 21:09:31 -0300

    ProjectAftermath - v1.0.2 UPDATE 2

--- Aftermath/src/script.js
+++ Aftermath/src/script.js
@@ -1,6 +1,6 @@
 function aboutPanel() {
   window.alert(
-    "ProjectAftermath. Versão 1.0.2 UPDATE 1. Desenvolvido por André Pereira(@Redwars22) usando HTML, CSS, Fluent UI e JavaScript."
+    "ProjectAftermath. Versão 1.0.2 UPDATE 2. Desenvolvido por André Pereira(@Redwars22) usando HTML, CSS, Fluent UI e JavaScript."
   );
 }
 
